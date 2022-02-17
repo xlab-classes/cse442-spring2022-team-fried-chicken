799b3a29c8b3fbffe40397a59f1c3e16a005ee4d
safriedl <60751010+safriedl@users.noreply.github.com>
2022-02-17 18:59:25 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from discord.ext import commands
 intents = discord.Intents.default()  # Intents required to be declared for certain server perms
 intents.guilds = True
 intents.members = True
-bot = commands.Bot(command_prefix='b!', help_command=None, intents=intents)  # Creates instance of bots
+bot = commands.Bot(command_prefix='%', help_command=None, intents=intents)  # Creates instance of bots
 
 bot.run('TOKEN_HERE', bot=True)  # Starts the bot
