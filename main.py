74b280d45ef37a3bbcc4c2df06e82552735a0f76
safriedl <60751010+safriedl@users.noreply.github.com>
2022-02-18 15:16:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,4 +6,21 @@ intents.guilds = True
 intents.members = True
 bot = commands.Bot(command_prefix='%', help_command=None, intents=intents)  # Creates instance of bots
 
-bot.run('TOKEN_HERE', bot=True)  # Starts the bot
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+    await bot.change_presence(activity=discord.Game(name="stonks"))
+
+
+@bot.command()
+async def hello(ctx, num: int):
+    await ctx.send("hello {}".format(num + 1))
+
+
+@bot.command()
+async def JOHNCENA(ctx):
+    await ctx.send("YOU CAN'T SEE ME")
+
+
+bot.run(open("token.txt", "r").readline())  # Starts the bot
