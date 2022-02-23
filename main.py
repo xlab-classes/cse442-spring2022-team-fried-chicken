a4f2857e8b987ca59e8e78bedf824bd925c393af
safriedl <60751010+safriedl@users.noreply.github.com>
2022-02-23 15:37:23 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import random
 
 intents = discord.Intents.default()  # Intents required to be declared for certain server perms
 intents.guilds = True
@@ -23,4 +24,22 @@ async def JOHNCENA(ctx):
     await ctx.send("YOU CAN'T SEE ME")
 
 
+@bot.command()
+async def assign_roles(ctx):
+    players = []
+    for member in ctx.guild.members:
+        if not member.bot:
+            players.append(member)
+    hitler = random.sample(players, 1)[0]
+    fascist = random.sample(players, 1)[0]
+    hitler_dm = await hitler.create_dm()
+    await hitler_dm.send("You are hitler!")
+    fascist_dm = await fascist.create_dm()
+    await fascist_dm.send("You are fascist!")
+    for liberal in players:
+        liberal_dm = await liberal.create_dm()
+        await liberal_dm.send("You are liberal!")
+
+
 bot.run(open("token.txt", "r").readline())  # Starts the bot
+
